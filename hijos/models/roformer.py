498670418a92f56b0e1c309962990a4d61d70ccf
Pedro Cordoba Gonzalez <pcordoba@alicebiometrics.com>
2024-04-23 22:27:07 -0400

    feat(TorchBSRoformer): add model

---
+++ hijos/models/roformer.py
@@ -0,0 +1,16 @@
+import torch
+from bs_roformer import BSRoformer
+from madre.models.torch_base_model import TorchBaseModel
+
+
+class TorchBSRoformer(TorchBaseModel):
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.model = BSRoformer(
+            dim=512, depth=12, time_transformer_depth=1, freq_transformer_depth=1
+        )
+
+    def inference(self, input: torch.Tensor) -> torch.Tensor:
+        return self.model(input)
