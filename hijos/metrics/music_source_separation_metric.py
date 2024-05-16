3cf0a6bed344d9c7076c14243084b6afa97282f3
Pedro Cordoba Gonzalez <pcordoba@alicebiometrics.com>
2024-05-15 20:29:58 -0400

    feat(MusicSourceSeparationMetric): use museval

--- hijos/metrics/music_source_separation_metric.py
+++ hijos/metrics/music_source_separation_metric.py
@@ -1,8 +1,8 @@
 import numpy as np
+from einops import rearrange
 from madre import register
 from madre.evaluation.metrics.metric import Metric
-from mir_eval import separation
-from torch import Tensor
+from museval import evaluate
 
 STEMS = ["mixture", "vocals", "drums", "bass", "other"]
 
@@ -20,21 +20,40 @@ class MusicSourceSeparationMetric(Metric):
             self.targets = targets
         self._init_metrics()
 
-    def _init_metrics(self):
-        self.metrics: dict[str, list[float]] = {"SDR": [], **{f"SDR_{target}": [] for target in self.targets}}
+    def _init_metrics(self) -> None:
+        self.metrics: dict[str, list[float]] = {
+            "SDR": [],
+            **{f"SDR_{target}": [] for target in self.targets},
+            "ISR": [],
+            **{f"ISR_{target}": [] for target in self.targets},
+            "SIR": [],
+            **{f"SIR_{target}": [] for target in self.targets},
+            "SAR": [],
+            **{f"SAR_{target}": [] for target in self.targets},
+        }
 
-    def calculate(self, batch_inputs: Tensor, batch_targets: Tensor) -> None:
+    def calculate(self, batch_inputs: np.ndarray, batch_targets: np.ndarray) -> None:
+        breakpoint()
         if batch_inputs.ndim == 3:
             # add batch dimension
             batch_inputs = batch_inputs[np.newaxis]
             batch_targets = batch_targets[np.newaxis]
+
         for inputs, targets in zip(batch_inputs, batch_targets, strict=True):
-            sdr_sources = []
-            for i, (input, target) in enumerate(zip(inputs, targets, strict=True)):
-                # 0 -> SDR, 1 -> SIR, 2 -> SAR
-                sdr = separation.bss_eval_sources(target, input)[0].mean()
-                self.metrics[f"SDR_{self.targets[i]}"].append(sdr)
-            self.metrics["SDR"].append(np.mean(sdr_sources))
+            inputs = rearrange(inputs, "nsrc nchan nsampl -> nsrc nsampl nchan")
+            targets = rearrange(targets, "nsrc nchan nsampl -> nsrc nsampl nchan")
+            sdr, isr, sir, sar = evaluate(targets, inputs)
+            for i, (sdr_stem, isr_stem, sir_stem, sar_stem) in enumerate(
+                zip(sdr, isr, sir, sar, strict=True)
+            ):
+                self.metrics[f"SDR_{self.targets[i]}"].append(np.nanmean(sdr_stem))
+                self.metrics[f"ISR_{self.targets[i]}"].append(np.nanmean(isr_stem))
+                self.metrics[f"SIR_{self.targets[i]}"].append(np.nanmean(sir_stem))
+                self.metrics[f"SAR_{self.targets[i]}"].append(np.nanmean(sar_stem))
+            self.metrics["SDR"].append(np.nanmean(sdr))
+            self.metrics["ISR"].append(np.nanmean(isr))
+            self.metrics["SDR"].append(np.nanmean(sir))
+            self.metrics["ISR"].append(np.nanmean(sar))
 
     def log(self, epoch: int | None = None) -> None:
         for metric_name, metric in self.metrics.items():
