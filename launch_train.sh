11c9a18b88ed952fddfeb3537985f3f1ab759017
pedrocg42 <pcordoba@alicebiometrics.com>
2024-05-21 03:01:43 +0000

    chore(launc_train): interleave models

--- launch_train.sh
+++ launch_train.sh
@@ -1,12 +1,15 @@
 export DATASETS_PATH="/home/pcordoba/data/audio/"
 
-madre-experiment experiments/bs-roformer_vocals.yaml
-madre-experiment experiments/bs-roformer_bass.yaml
-madre-experiment experiments/bs-roformer_drums.yaml
-madre-experiment experiments/bs-roformer_others.yaml
+madre-experiment experiments/bs-roformer/bs-roformer_vocals.yaml
+madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_vocals.yaml
 
-madre-experiment experiments/bs-mel-roformer_vocals.yaml
-madre-experiment experiments/bs-mel-roformer_bass.yaml
-madre-experiment experiments/bs-mel-roformer_drums.yaml
-madre-experiment experiments/bs-mel-roformer_others.yaml
+madre-experiment experiments/bs-roformer/bs-roformer_bass.yaml
+madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_bass.yaml
+
+madre-experiment experiments/bs-roformer/bs-roformer_drums.yaml
+madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_drums.yaml
+
+madre-experiment experiments/bs-roformer/bs-roformer_others.yaml
+madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_others.yaml
+
 sudo shutdown
