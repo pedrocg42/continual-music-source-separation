cf775f55d74cdebc4a09d684c95b5cebb6448a58
pedrocg42 <pcordoba@alicebiometrics.com>
2024-05-25 23:30:32 +0000

    chore(experiments): demucs basic experiments

--- launch_train.sh
+++ launch_train.sh
@@ -1,15 +1,8 @@
 export DATASETS_PATH="/home/pcordoba/data/audio/"
 
-madre-experiment experiments/bs-roformer/bs-roformer_vocals.yaml
-madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_vocals.yaml
-
-madre-experiment experiments/bs-roformer/bs-roformer_bass.yaml
-madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_bass.yaml
-
-madre-experiment experiments/bs-roformer/bs-roformer_drums.yaml
-madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_drums.yaml
-
-madre-experiment experiments/bs-roformer/bs-roformer_others.yaml
-madre-experiment experiments/bs-mel-roformer/bs-mel-roformer_others.yaml
+madre-experiment experiments/demucs/demucs_vocals.yaml
+madre-experiment experiments/demucs/demucs_bass.yaml
+madre-experiment experiments/demucs/demucs_drums.yaml
+madre-experiment experiments/demucs/demucs_other.yaml
 
 sudo shutdown
