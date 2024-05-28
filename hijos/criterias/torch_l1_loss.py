98fba1d30d425eb97ba0ddff3b5e79f74b0ac844
pedrocg42 <pcordoba@alicebiometrics.com>
2024-05-28 02:12:20 +0000

    experiments(demucs_adam): preprare new adam experiments

--- hijos/criterias/torch_l1_loss.py
+++ hijos/criterias/torch_l1_loss.py
@@ -9,5 +9,5 @@ class TorchSourceSeparationL1Criteria(TorchCriteria):
     def forward(self, recon_audio: Tensor, target: Tensor) -> dict[str, Tensor]:
         dims = tuple(range(2, recon_audio.dim()))
         loss = F.l1_loss(recon_audio, target, reduction="none")
-        loss = loss.mean(dims).mean(0)
+        loss = loss.mean(dims).mean(1).mean(0)
         return {"loss": loss}
--- launch_train.sh
+++ launch_train.sh
@@ -1,8 +1,14 @@
 export DATASETS_PATH="/home/pcordoba/data/audio/"
 
-madre-experiment experiments/demucs/demucs_vocals.yaml
-madre-experiment experiments/demucs/demucs_bass.yaml
-madre-experiment experiments/demucs/demucs_drums.yaml
-madre-experiment experiments/demucs/demucs_other.yaml
+# madre-experiment experiments/demucs/demucs_vocals.yaml
+# madre-experiment experiments/demucs/demucs_bass.yaml
+# madre-experiment experiments/demucs/demucs_drums.yaml
+# madre-experiment experiments/demucs/demucs_other.yaml
+
+madre-experiment experiments/demucs_adam/demucs_all.yaml
+madre-experiment experiments/demucs_adam/demucs_vocals.yaml
+madre-experiment experiments/demucs_adam/demucs_bass.yaml
+madre-experiment experiments/demucs_adam/demucs_drums.yaml
+madre-experiment experiments/demucs_adam/demucs_other.yaml
 
 sudo shutdown
